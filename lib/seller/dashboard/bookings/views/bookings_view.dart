3863cd3e0e994889b9d59613a69af2befc8244dd
muhammadarslanapax <arslanapax@gmail.com>
2024-02-29 16:29:20 +0500

    update_1.1

---
+++ lib/seller/dashboard/bookings/views/bookings_view.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:home_brigadier/consts/media_query.dart';
+import 'package:home_brigadier/generated/locales.g.dart';
+import 'package:home_brigadier/widget/cText.dart';
+
+import '../../../../app/seller/dashboard/bookings/controllers/bookings_controller.dart';
+import '../../../../app/seller/dashboard/profile/user_profile/views/bookings_view.dart';
+
+class BookingsView extends GetView<BookingsController> {
+  const BookingsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Get.put(BookingsController());
+    final txtTheme = Theme.of(context).textTheme.titleMedium!.color;
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: Center(child: Image.asset("assets/images/ic_splash.png"))),
+        ),
+        title: CText(
+            text: LocaleKeys.dashboard_items_bookings.tr,
+            fontWeight: FontWeight.bold,
+            color: txtTheme,
+            fontsize: mediaQueryHeight(context) * 0.025),
+      ),
+      body: const MyBookingsView(),
+    );
+
+    // GetBuilder<BookingsController>(
+    //   init: Get.put(BookingsController()),
+    //   builder: (builderContext) {
+    //     return DefaultTabController(
+    //         length: 3,
+    //         child: Scaffold(
+    //             appBar: AppBar(
+    //                 leading: Container(
+    //                     margin: const EdgeInsets.all(10),
+    //                     padding: const EdgeInsets.all(8.0),
+    //                     decoration: const BoxDecoration(
+    //                         borderRadius:
+    //                             BorderRadius.all(Radius.circular(12)),
+    //                         color: AppColor.secondary),
+    //                     child: SvgPicture.asset("assets/icons/ic_logo.svg")),
+    //                 title: CText(
+    //                     text: "My Bookings",
+    //                     fontWeight: FontWeight.bold,
+    //                     color: txtTheme,
+    //                     fontsize: mediaQueryHeight(context) * 0.025),
+    //                 actions: [
+    //                   CIconBtn(
+    //                       onPressed: () {},
+    //                       child: SvgPicture.asset(
+    //                           "assets/icons/ic_search.svg",
+    //                           color: svgTheme)),
+    //                   CIconBtn(
+    //                       onPressed: () {},
+    //                       child: SvgPicture.asset(
+    //                           "assets/icons/ic_more_outlined.svg",
+    //                           color: svgTheme))
+    //                 ],
+    //                 bottom: TabBar(
+    //                     padding: EdgeInsets.symmetric(
+    //                         horizontal: mediaQueryWidth(context) * 0.05),
+    //                     automaticIndicatorColorAdjustment: true,
+    //                     indicatorWeight: 3,
+    //                     tabs: const [
+    //                       Tab(text: 'Upcoming'),
+    //                       Tab(text: 'Completed'),
+    //                       Tab(text: 'Cancelled')
+    //                     ])),
+    //             body: const TabBarView(children: [
+    //               UpcomingView(),
+    //               CompletedView(),
+    //               CancelledView()
+    //             ])));
+    //   });
+  }
+}
---
+++ lib/seller/dashboard/views/dashboard_view.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:home_brigadier/consts/app_color.dart';
+import 'package:home_brigadier/consts/media_query.dart';
+
+import '../../../../generated/locales.g.dart';
+import '../../../app/seller/dashboard/controllers/dashboard_controller.dart';
+import '../../../app/user/dashboard/home/controllers/home_controller.dart';
+
+class SellerDashboardView extends GetView<DashboardController> {
+  const SellerDashboardView({Key? key}) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<DashboardController>(
+        init: DashboardController(),
+        builder: (builderContext) {
+          return Scaffold(
+            ///body with pageBuilder
+            body: IndexedStack(
+              index: controller.currentIndex.value,
+              children: controller.pages,
+            ),
+
+            /// bottomNavigationBar
+            bottomNavigationBar: GetBuilder<HomeController>(builder: (obj) {
+              return
+                   BottomNavigationBar(
+                    selectedLabelStyle:  TextStyle(overflow: TextOverflow.visible,fontSize: mediaQueryHeight(context)*0.015),
+      unselectedLabelStyle:  TextStyle(overflow: TextOverflow.visible,fontSize: mediaQueryHeight(context)*0.015),
+                      type: BottomNavigationBarType.fixed,
+                      fixedColor: AppColor.secondary,
+                      currentIndex: controller.currentIndex.value,
+                      onTap: (index) => controller.changePage(index),
+                      items: [
+                        // BottomNavigationBarItem(
+                          
+                          
+                        //   icon: SvgPicture.asset("assets/icons/ic_home_outlined.svg"),
+                        //   activeIcon: SvgPicture.asset("assets/icons/ic_home_filled.svg"),
+                        //   label: LocaleKeys.dashboard_items_home.tr,
+                        // ),
+                        BottomNavigationBarItem(
+                          icon: SvgPicture.asset("assets/icons/ic_booking_outlined.svg"),
+                          activeIcon: SvgPicture.asset("assets/icons/ic_booking_filled.svg"),
+                          label: LocaleKeys.dashboard_items_bookings.tr,
+                        ),
+                        BottomNavigationBarItem(
+                          icon: const Icon(Icons.currency_exchange,),
+                          activeIcon: const Icon(
+                            
+                            Icons.currency_exchange,
+                            color: AppColor.secondary,
+                          ),
+                          label: LocaleKeys.dashboard_items_start_earning.tr,
+                        ),
+                        BottomNavigationBarItem(
+                          icon: const Icon(Icons.question_mark),
+                          activeIcon: const Icon(Icons.question_mark),
+                          label: LocaleKeys.dashboard_items_faq.tr,
+                        ),
+                        BottomNavigationBarItem(
+                          icon: SvgPicture.asset("assets/icons/ic_profile_circled_outlined.svg"),
+                          activeIcon:
+                              SvgPicture.asset("assets/icons/ic_profile_circled_filled.svg"),
+                          label: LocaleKeys.dashboard_items_profile.tr,
+                        ),
+                      ],
+                    );
+                 
+                            
+            }),
+          );
+        });
+  }
+}
